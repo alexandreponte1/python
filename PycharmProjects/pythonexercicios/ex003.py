86de1dda46e9031c033fa3ffc94b16dbf70da562
alexandreponte1 <uaspont@gmail.com>
2021-03-21 16:26:40 -0300

    first commit

---
+++ PycharmProjects/pythonexercicios/ex003.py
@@ -0,0 +1,12 @@
+n1 = float(input('Digite um valor: '))
+n2 = int(input('Digite um valor: '))
+s = n1 + n2
+print(' A soma entre {} e {} é igual a {}'.format(n1, n2, s))
+
+
+#n1 = int(input('Digite um valor: '))
+#n2 = int(input('Digite um valor: '))
+#print('A soma entre', n1, 'e', n2,  'vale', s) Esse é do formato antigo do python
+#s = n1 + n2
+#print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+
