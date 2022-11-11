15b4dac7afec876ea99c6be2ed81e29cca722b88
alexandreponte1 <uaspont@gmail.com>
2022-11-11 16:32:11 -0300

    add api exemplo

---
+++ API/consumir.py
@@ -0,0 +1,15 @@
+import requests
+
+link = 'https://minhaapi.alexandreponte.repl.co/pegarvendas'
+requisicao = requests.get(link)
+
+print(requisicao)
+print(requisicao.json())
+
+dicionario = requisicao.json()
+
+print(dicionario['total_vendas'])
+
+
+
+# https://minhaapi.alexandreponte.repl.co/pegarvendas
---
+++ API/readme.md
@@ -0,0 +1,1 @@
+Estou utilizando o https://replit.com para subir a api.
