965cc2bd0a5abbc600eb2c219017c18b382f9e57
alexandreponte1 <alexandre.s.ponte@concrete.com.br>
2022-12-13 15:55:30 -0300

    add gcp function with jenkins file

--- alura/OO/OO2/modelo.py
+++ alura/OO/OO2/modelo.py
@@ -1,26 +1,40 @@
-class Filme:
-	def __init__(self, nome, ano, duracao):
-		self.nome = nome
-		self.ano = ano
-		self.duracao = duracao
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title() # __Programa__nome
+        self.ano = ano
+        self._likes = 0
 
+    @property
+    def likes(self):
+        return self._likes
 
-class Serie:
-	def __init__(self, nome, ano, temporadas):
-		self.nome = nome
-		self.ano = ano
-		self.temporadas = temporadas
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+#######
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
 
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_likes()
+print(f'{vingadores.nome} - {vingadores.duracao}')
 
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duracao {vingadores.duracao}')
-
-
-
-
-
-fringe = Serie('Fringe', 2008, 5)
-
-print(f'Nome: {fringe.nome} - Ano: {fringe.ano} - Temporadas {fringe.temporadas}')
+fringe = Serie('fringe', 2008, 5)
+print(f'Nome: {fringe.nome} - Ano: {fringe.ano}')
