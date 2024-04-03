97da5ea794c8234fe1433a2965471a1d206e48bd
Alexandre <uaspont@gmail.com>
2024-04-03 18:18:00 -0300

    update

---
+++ inicio/curso_python/aula1.py
@@ -0,0 +1,11 @@
+"""
+docString  stream entre 3 aspas.
+O interpretador do python lê isso.
+
+docString pode ter varias linhas.
+"""
+
+
+# permite escrever comentário.
+print("oi eu sou o goku")  # na frente
+print(123)
---
+++ inicio/curso_python/aula3.py
@@ -0,0 +1,25 @@
+"""
+DocString, tamém é um tipo de string
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+
+print(1234)
+
+# Aspas simples
+print('Alexandre Ponte')
+print(1,'"Alexandre Ponte"')
+
+
+# Aspas duplas
+print(2, "'Alexandre Ponte'")
+
+# Escape
+print("Alexandre \"Ponte\"")
+
+
+# r
+print(r"Alexandre \"Ponte\"")
