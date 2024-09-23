280591a883f52eb5a089b0b0cd985e00e6909911
alexandreponte1 <alexandre.s.ponte@accenture.com.br>
2024-09-23 15:31:07 -0300

    update

---
+++ plural/aws_resources/aws_s3.py
@@ -0,0 +1,32 @@
+import boto3
+
+aws_access_key = ""
+aws_secret_key = ""
+aws_region = "us-east-1"
+
+bucket_name = "ps-course-test-bucket-gokuuuuuiiiooou"
+
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key,
+    region_name=aws_region
+)
+
+def get_location_constraint(region_name):
+    if region_name == "us-east-1":
+        return ""
+    else:
+        return region_name
+
+def create_s3_bucket(bucket_name):
+    response = s3_client.create_bucket(Bucket=bucket_name)
+    print(f"S3 Bucket '{bucket_name}' created successfully")
+    print(response)
+
+def delete_s3_bucket(bucket_name):
+    response = s3_client.delete_bucket(Bucket=bucket_name)
+    print(f"S3 Bucket '{bucket_name}' deleted successfully")
+
+if __name__ == "__main__":
+    delete_s3_bucket(bucket_name)
