27cd09627e5084c430f5856b216e2aefc09a635f
alexandreponte2 <alexandre.s.ponte@accenture.com>
2025-03-26 20:11:24 -0300

    teste

---
+++ intro/Built-in Functions/dict.py
@@ -0,0 +1,27 @@
+# mydict = {"key1":"value1", "key2":"value2", "key3":"value3"}
+
+# print(mydict["key2"])
+
+# print(mydict.items)
+
+# prices_lookup = {"apple":2.99, "oranges":1.99, "milk":5.80 }
+
+# prices_lookup["apple"]
+
+d = {"key1":["a","b","c"]}
+
+print(d["key1"][2].upper())
+
+# d = {"k1":100, "k2":200}
+
+# d["k3"] = 300
+
+# print(d)
+
+# d["k1"] = "NEW VALUE"
+
+# print(d)
+
+d = {'k1': 100, 'k2': 200, 'k3': 300}
+
+print(d.values())
